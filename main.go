19e314b4acda57248eda688ba747cdddcac62f65
firLambda <koki19991612@gmail.com>
2025-01-31 15:22:50 +0900

    add a getUser function into user.go

--- main.go
+++ main.go
@@ -13,12 +13,15 @@ func main() {
 
 	// log.Println("test")
 
-	fmt.Println(models.Db)
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
-	fmt.Println(u)
+	// fmt.Println(models.Db)
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@example.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
 
-	u.CreateUser()
+	// u.CreateUser()
+
+	u, _ := models.GetUser(1)
+	fmt.Println(u)
 }
